8821c0834bb00d05fb628b7287ed4f2afdeb5ead
maniac22 <mphomathabatha15@gmail.com>
2019-02-27 15:05:09 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,5 +1,13 @@
-def rev(Str):
-	if len(Str)==0:return Str;
-	else:return Str[len(Str)-1]+rev(Str[0:len(Str)-1])
-x=str(input())
-print(rev(x));
+import unittest
+
+class TestMethods(unittest.TestCase):
+	def test_rev(self):
+        	self.assertEqual(self.rev("TUNA"), "ANUT")
+	def rev(self,Str):
+		if len(Str)==0:return Str;
+		else:return Str[len(Str)-1]+self.rev(Str[0:len(Str)-1])
+
+if __name__ == '__main__':
+    unittest.main()
+
+
