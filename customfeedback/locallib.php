af01159136949e5d9f4dd84c925eabff8dc4a764
mphohlabirwa mathabatha <1710409@host006-02-07.ms.wits.ac.za>
2019-03-07 21:42:30 +0200

    Added couple of functions

--- customfeedback/locallib.php
+++ customfeedback/locallib.php
@@ -40,5 +40,49 @@ class assign_feedback_file extends assign_feedback_plugin{
 
         return true;
     }
+    public function is_feedback_modified(stdClass $grade, stdClass $data) {
+        $commenttext = '';
+        if ($grade) {
+            $feedbackcomments = $this->get_feedback_comments($grade->id);
+            if ($feedbackcomments) {
+                $commenttext = $feedbackcomments->commenttext;
+            }
+        }
+
+        if ($commenttext == $data->assignfeedbackcomments_editor['text']) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public function save(stdClass $grade, stdClass $data) {
+        global $DB;
+
+        $fileoptions = $this->get_file_options();
+
+        $userid = $grade->userid;
+        $elementname = 'files_' . $userid;
+
+        $data = file_postupdate_standard_filemanager($data,
+            $elementname,
+            $fileoptions,
+            $this->assignment->get_context(),
+            'assignfeedback_file',
+            ASSIGNFEEDBACK_FILE_FILEAREA,
+            $grade->id);
+
+        $filefeedback = $this->get_file_feedback($grade->id);
+        if ($filefeedback) {
+            $filefeedback->numfiles = $this->count_files($grade->id, ASSIGNFEEDBACK_FILE_FILEAREA);
+            return $DB->update_record('assignfeedback_file', $filefeedback);
+        } else {
+            $filefeedback = new stdClass();
+            $filefeedback->numfiles = $this->count_files($grade->id, ASSIGNFEEDBACK_FILE_FILEAREA);
+            $filefeedback->grade = $grade->id;
+            $filefeedback->assignment = $this->assignment->get_instance()->id;
+            return $DB->insert_record('assignfeedback_file', $filefeedback) > 0;
+        }
+    }
 
 }
