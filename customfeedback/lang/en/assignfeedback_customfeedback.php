96dc2e64fdfaa1c31612c97b6b6b966f53c79220
moseskatlego modupi <1614669@host006-04-04.ms.wits.ac.za>
2019-03-07 20:17:25 +0200

    msg

---
+++ customfeedback/lang/en/assignfeedback_customfeedback.php
@@ -0,0 +1,26 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *
+ * @package   assignfeedback_customfeedback
+ * @copyright 2019, Moses Modupi <1614669@student.wits.ac.za>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Awesome feedback';
+$string['enabled'] = 'Awesome feedback';
+$string['enabled_help'] = 'If enabled, the teacher will be able to provide awesome feedback ';
