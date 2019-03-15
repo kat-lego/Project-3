39f7adfee7a40d3b0fa41154e0925302a38905d6
1710409 <1710409@students.wits.ac.za>
2019-03-15 16:12:12 +0200

    Moved

---
+++ lib.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1614669
+ * Date: 2019/03/07
+ * Time: 7:53 PM
+ */
---
+++ locallib.php
@@ -0,0 +1,276 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1614669
+ * Date: 2019/03/07
+ * Time: 9:01 PM
+ */
+class assign_feedback_customfeedback extends assign_feedback_plugin{
+
+
+    public function get_name() {
+        return get_string('pluginname', 'assignfeedback_customfeedback');
+    }
+
+
+    public function get_settings(MoodleQuickForm $mform) {
+        $mform->addElement('assignfeedback_file_fileextensions', get_string('allowedfileextensions', 'assignfeedback_customfeedback'));
+        $mform->setType('assignfeedback_customfeedback_fileextensions', PARAM_FILE);
+    }
+
+
+    public function save_settings(stdClass $data) {
+        $this->set_config('allowedfileextensions', $data->allowedfileextensions);
+        return true;
+    }
+
+    public function get_form_elements_for_user($grade, MoodleQuickForm $mform, stdClass $data, $userid) {
+
+        $fileoptions = $this->get_file_options();
+        $gradeid = $grade ? $grade->id : 0;
+        $elementname = 'files_' . $userid;
+
+        $data = file_prepare_standard_filemanager($data,
+                                                  $elementname,
+                                                  $fileoptions,
+                                                  $this->assignment->get_context(),
+                                                  'assignfeedback_customfeedback',
+                                                  ASSIGNFEEDBACK_FILE_FILEAREA,
+                                                  $gradeid);
+        $mform->addElement('filemanager', $elementname . '_filemanager', html_writer::tag('span', $this->get_name(),
+            array('class' => 'accesshide')), null, $fileoptions);
+
+        return true;
+    }
+    public function is_feedback_modified(stdClass $grade, stdClass $data) {
+        $commenttext = '';
+        if ($grade) {
+            $feedbackcomments = $this->get_feedback_comments($grade->id);
+            if ($feedbackcomments) {
+                $commenttext = $feedbackcomments->commenttext;
+            }
+        }
+
+        if ($commenttext == $data->assignfeedbackcomments_editor['text']) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public function save(stdClass $grade, stdClass $data) {
+        global $DB;
+
+        $fileoptions = $this->get_file_options();
+
+        $userid = $grade->userid;
+        $elementname = 'files_' . $userid;
+
+        $data = file_postupdate_standard_filemanager($data,
+            $elementname,
+            $fileoptions,
+            $this->assignment->get_context(),
+            'assignfeedback_file',
+            ASSIGNFEEDBACK_FILE_FILEAREA,
+            $grade->id);
+
+        $filefeedback = $this->get_file_feedback($grade->id);
+        if ($filefeedback) {
+            $filefeedback->numfiles = $this->count_files($grade->id, ASSIGNFEEDBACK_FILE_FILEAREA);
+            return $DB->update_record('assignfeedback_file', $filefeedback);
+        } else {
+            $filefeedback = new stdClass();
+            $filefeedback->numfiles = $this->count_files($grade->id, ASSIGNFEEDBACK_FILE_FILEAREA);
+            $filefeedback->grade = $grade->id;
+            $filefeedback->assignment = $this->assignment->get_instance()->id;
+            return $DB->insert_record('assignfeedback_file', $filefeedback) > 0;
+        }
+    }
+
+    public function view_summary(stdClass $grade, & $showviewlink) {
+        $count = $this->count_files($grade->id, ASSIGNFEEDBACK_FILE_FILEAREA);
+        // show a view all link if the number of files is over this limit
+        $showviewlink = $count > ASSIGNFEEDBACK_FILE_MAXSUMMARYFILES;
+
+        if ($count <= ASSIGNFEEDBACK_FILE_MAXSUMMARYFILES) {
+            return $this->assignment->render_area_files('assignfeedback_file', ASSIGNFEEDBACK_FILE_FILEAREA, $grade->id);
+        } else {
+            return get_string('countfiles', 'assignfeedback_file', $count);
+        }
+    }
+
+    public function view(stdClass $grade) {
+        return $this->assignment->render_area_files('assignfeedback_file', ASSIGNFEEDBACK_FILE_FILEAREA, $grade->id);
+    }
+
+
+    public function can_upgrade($type, $version) {
+
+        if (($type == 'upload' || $type == 'uploadsingle') && $version >= 2011112900) {
+            return true;
+        }
+        return false;
+    }
+
+
+
+    public function upgrade_settings(context $oldcontext, stdClass $oldassignment, & $log) {
+        // first upgrade settings (nothing to do)
+        return true;
+    }
+
+    public function upgrade(context $oldcontext, stdClass $oldassignment, stdClass $oldsubmission, stdClass $grade, & $log) {
+        global $DB;
+
+        // now copy the area files
+        $this->assignment->copy_area_files_for_upgrade($oldcontext->id,
+            'mod_assignment',
+            'response',
+            $oldsubmission->id,
+            // New file area
+            $this->assignment->get_context()->id,
+            'assignfeedback_file',
+            ASSIGNFEEDBACK_FILE_FILEAREA,
+            $grade->id);
+
+        // now count them!
+        $filefeedback = new stdClass();
+        $filefeedback->numfiles = $this->count_files($grade->id, ASSIGNFEEDBACK_FILE_FILEAREA);
+        $filefeedback->grade = $grade->id;
+        $filefeedback->assignment = $this->assignment->get_instance()->id;
+        if (!$DB->insert_record('assignfeedback_file', $filefeedback) > 0) {
+            $log .= get_string('couldnotconvertgrade', 'mod_assign', $grade->userid);
+            return false;
+        }
+        return true;
+    }
+
+
+
+    public function is_empty(stdClass $submission) {
+        return $this->count_files($submission->id, ASSIGNSUBMISSION_FILE_FILEAREA) == 0;
+    }
+
+
+    public function get_file_areas() {
+        return array(ASSIGNFEEDBACK_FILE_FILEAREA=>$this->get_name());
+    }
+
+
+
+    public function delete_instance() {
+        global $DB;
+        // will throw exception on failure
+        $DB->delete_records('assignfeedback_file', array('assignment'=>$this->assignment->get_instance()->id));
+
+        return true;
+    }
+
+
+
+    public function format_for_gradebook(stdClass $grade) {
+        return FORMAT_MOODLE;
+    }
+
+
+
+    public function text_for_gradebook(stdClass $grade) {
+        return '';
+    }
+
+    /**
+     * Override to indicate a plugin supports quickgrading
+     *
+     * @return boolean - True if the plugin supports quickgrading
+     */
+    public function supports_quickgrading() {
+        return false;
+    }
+
+    /**
+     * Get quickgrading form elements as html
+     *
+     * @param int $userid The user id in the table this quickgrading element relates to
+     * @param mixed $grade grade or null - The grade data. May be null if there are no grades for this user (yet)
+     * @return mixed - A html string containing the html form elements required for quickgrading or false to indicate this plugin does not support quickgrading
+     */
+    public function get_quickgrading_html($userid, $grade) {
+        return false;
+    }
+
+    /**
+     * Has the plugin quickgrading form element been modified in the current form submission?
+     *
+     * @param int $userid The user id in the table this quickgrading element relates to
+     * @param stdClass $grade The grade
+     * @return boolean - true if the quickgrading form element has been modified
+     */
+    public function is_quickgrading_modified($userid, $grade) {
+        return false;
+    }
+
+    /**
+     * Save quickgrading changes
+     *
+     * @param int $userid The user id in the table this quickgrading element relates to
+     * @param stdClass $grade The grade
+     * @return boolean - true if the grade changes were saved correctly
+     */
+    public function save_quickgrading_changes($userid, $grade) {
+        return false;
+    }
+
+
+    /**
+     * Run cron for this plugin
+     */
+    public static function cron() {
+
+    }
+
+    /**
+     * Return a list of the grading actions supported by this plugin.
+     *
+     * A grading action is a page that is not specific to a user but to the whole assignment.
+     * @return array - An array of action and description strings.
+     *                 The action will be passed to grading_action.
+     */
+    public function get_grading_actions() {
+        return array();
+    }
+
+    /**
+     * Show a grading action form
+     *
+     * @param string $gradingaction The action chosen from the grading actions menu
+     * @return string The page containing the form
+     */
+    public function grading_action($gradingaction) {
+        return '';
+    }
+
+    /**
+     * Return a list of the batch grading operations supported by this plugin.
+     *
+     * @return array - An array of action and description strings.
+     *                 The action will be passed to grading_batch_operation.
+     */
+    public function get_grading_batch_operations() {
+        return array();
+    }
+
+    /**
+     * Show a batch operations form
+     *
+     * @param string $action The action chosen from the batch operations menu
+     * @param array $users The list of selected userids
+     * @return string The page containing the form
+     */
+    public function grading_batch_operation($action, $users) {
+        return '';
+    }
+
+
+
+
+}
