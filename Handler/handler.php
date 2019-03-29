595cee16b73c6df3eba312b272e419525ffa3623
maniac22 <mphomathabatha15@gmail.com>
2019-03-29 23:14:00 +0200

    Assignment handler with minimal functionality

--- Handler/handler.php
+++ Handler/handler.php
@@ -1,4 +1,22 @@
 <?php
 
 
+$inputJSON = file_get_contents('php://input');
+$input = json_decode($inputJSON, TRUE);        
+$source = base64_decode($input['source']);     
+$mode=$input["mode"];
+
+if(!isset($input["mode"])){
+	die("No mode selected");
+}
+
+/**
+*do stuff based on the mode type
+*output contains the grade and whether the output of the code is correct
+*/
+$output= array('grade' =>random_int(0, 100),'passed'=>TRUE);
+
+
+echo json_encode($output);
+?>
 
