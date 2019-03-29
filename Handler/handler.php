a0147a09027c9c6f96d722d32c073fa5538c9091
maniac22 <mphomathabatha15@gmail.com>
2019-03-29 23:16:33 +0200

    Assignment handler with minimal functionality

--- Handler/handler.php
+++ Handler/handler.php
@@ -1,17 +1,20 @@
 <?php
 
+require_once("driver.php");//contains cool functions 
 
 $inputJSON = file_get_contents('php://input');
 $input = json_decode($inputJSON, TRUE);        
 $source = base64_decode($input['source']);     
-$mode=$input["mode"];
+$limit=$input['timelimit'];
+$mode=$input['mode'];
+
 
 if(!isset($input["mode"])){
 	die("No mode selected");
 }
 
 /**
-*do stuff based on the mode type
+*do call functions to do stuff based on the mode type
 *output contains the grade and whether the output of the code is correct
 */
 $output= array('grade' =>random_int(0, 100),'passed'=>TRUE);
