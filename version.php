29b06d068aacbd52b64d05e8c34d5911ab4f071c
1637313 <1637313@students.wits.ac.za>
2019-04-06 12:53:26 +0000

    Database installation fix

--- version.php
+++ version.php
@@ -22,7 +22,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2019032406;
+$plugin->version = 2019032407;
 $plugin->requires = 2018120300; // Moodle 3.6
 $plugin->cron = 0;
 $plugin->component = 'assignfeedback_customfeedback';
