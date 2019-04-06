800b14ae2fbd25bf1b825bbca959c55f125daf4c
Katlego Modupi <47749050+kat-lego@users.noreply.github.com>
2019-04-07 01:54:11 +0200

    Update locallibTest.php

--- unitTests/locallibTest.php
+++ unitTests/locallibTest.php
@@ -10,10 +10,10 @@ class locallibTest extends TestCase{
         $this->assertEquals(2, 2); 
       }
   #it cant find the extended class in locallib.php
-     public function test_getmodes(){
-            #$tester=new assign_feedback_customfeedback;
-            #$result = $tester->getmodes();
-            #$expected = array("Classic Mode", "Fastest Mode", "Tournament Mode", "AI Mode");
+     public function test_get_modes(){
+            $tester=new assign_feedback_customfeedback;
+            $result = $tester->get_modes();
+            $expected = array("Classic Mode", "Fastest Mode", "Tournament Mode", "AI Mode");
             $this->assertEquals(2,2);
       }
 
