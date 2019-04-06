ec277421423b73528c545ee514b6eb1cdd615b7c
maniac22 <mphomathabatha15@gmail.com>
2019-04-07 00:53:11 +0200

    test get modes

--- unitTests/test_locallib.php
+++ unitTests/test_locallib.php
@@ -3,11 +3,17 @@
 
 use PHPUnit\Framework\TestCase;
 use PHPUnit\DbUnit\TestCaseTrait;
-#require_once('dev_1/witsoj/locallib.php');
+require_once('locallib.php');
 class locallibTest extends TestCase{
   
-      public function test_format_for_gradebook(){;
+      public function test_format_for_gradebook(){
         $this->assertEquals(2, 2); 
-    }
+      }
+      public function test_getmodes(){
+            $tester=new assign_feedback_customfeedback;
+            $result = $tester->getmodes();
+            $expected = array("Classic Mode", "Fastest Mode", "Tournament Mode", "AI Mode");
+            $this->assertEquals($expected,$result,"Correct");
+      }
 
 }
