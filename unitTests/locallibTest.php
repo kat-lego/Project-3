5f9b8f418f05820ae00226c156ecd17b0e513603
Katlego Modupi <47749050+kat-lego@users.noreply.github.com>
2019-04-07 01:57:40 +0200

    Update locallibTest.php

--- unitTests/locallibTest.php
+++ unitTests/locallibTest.php
@@ -7,14 +7,14 @@ use PHPUnit\DbUnit\TestCaseTrait;
 class locallibTest extends TestCase{
   
       public function test_format_for_gradebook(){
-        $this->assertEquals(2, 2); 
+        $this->assertEquals(2, 2,"correct!"); 
       }
   #it cant find the extended class in locallib.php
      public function test_get_modes(){
             $tester=new assign_feedback_customfeedback;
             $result = $tester->get_modes();
             $expected = array("Classic Mode", "Fastest Mode", "Tournament Mode", "AI Mode");
-            $this->assertEquals(2,2);
+            $this->assertEquals($expected,$result,"correct!");
       }
 
 }
