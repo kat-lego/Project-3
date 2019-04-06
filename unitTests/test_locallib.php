75f9fa80f8d16c5da0a50b3cf153b9aad5a1651f
maniac22 <mphomathabatha15@gmail.com>
2019-04-07 00:20:00 +0200

    New tests

---
+++ unitTests/test_locallib.php
@@ -0,0 +1,13 @@
+
+<?php
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\DbUnit\TestCaseTrait;
+#require_once('dev_1/witsoj/locallib.php');
+class locallibTest extends TestCase{
+  
+      public function test_format_for_gradebook(){;
+        $this->assertEquals(2, 2); 
+    }
+
+}
