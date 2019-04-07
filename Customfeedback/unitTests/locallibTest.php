32cb725134a4953924d972444fca14ee56869ced
maniac22 <mphomathabatha15@gmail.com>
2019-04-07 17:40:03 +0200

    Update locallibTest.php

--- Customfeedback/unitTests/locallibTest.php
+++ Customfeedback/unitTests/locallibTest.php
@@ -16,13 +16,13 @@ class locallibTest extends TestCase{
             $expected = array("Classic Mode", "Fastest Mode", "Tournament Mode", "AI Mode");
             $this->assertEquals($expected,$result,"correct!");
       }
-    public function test_ get_time_limits(){
+    public function test_get_time_limits(){
             $tester=new assign_feedback_customfeedback;
             $result = $tester->get_time_limits();
             $expected = array(1,3,5,10,20,60);
             $this->assertEquals($expected,$result,"correct!");
     }
-    public function test_ get_memory_limits(){
+    public function test_get_memory_limits(){
             $tester=new assign_feedback_customfeedback;
             $result = $tester->get_memory_limits();
             $expected = array('1MB','2MB','4MB','16MB');
