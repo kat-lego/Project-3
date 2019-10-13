3813418be2754bc35f651d80c62c1cff7e419361
kat-lego <1614669@students.wits.ac.za>
2019-10-13 04:28:54 +0000

    cleaned code a bit

--- db/upgrade.php
+++ db/upgrade.php
@@ -6,19 +6,8 @@ function xmldb_assignfeedback_customfeedback_upgrade($oldversion) {
     $result = TRUE;
  
 // Insert PHP code from XMLDB Editor here
-    if ($oldversion < 2019041402) {
-
-        // Define field question_number to be added to customfeedback_submission.
-        $table = new xmldb_table('customfeedback_submission');
-        $field = new xmldb_field('question_number', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null, null);
-
-        // Conditionally launch add field question_number.
-        if (!$dbman->field_exists($table, $field)) {
-            $dbman->add_field($table, $field);
-        }
-
-        // Customfeedback savepoint reached.
-        upgrade_plugin_savepoint(true, 2019041402, 'assignfeedback', 'customfeedback');
+    if ($oldversion < 2019121002) {
+        
     }
 
  
